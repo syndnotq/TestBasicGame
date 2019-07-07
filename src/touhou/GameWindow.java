ec3a76938f2b5bebcdea39cb9c1f740032fba4b4
syndnotq <oppneo72008@gmail.com>
2019-07-07 09:46:29 +0700

    Set Title

--- src/touhou/GameWindow.java
+++ src/touhou/GameWindow.java
@@ -23,6 +23,7 @@ public class GameWindow extends Frame {
 
     private void setupWindow() {
         this.setSize(1024, 768);
+        this.setTitle("Test Basic Game");
         this.setVisible(true);
         this.addWindowListener(new WindowAdapter() {
             @Override
